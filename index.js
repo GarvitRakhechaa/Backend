61ae33e5907d02b3a53cb012fd7c95c6eabcc5b2
GarvitRakhechaa <garvitrakhecha786@gmail.com>
2025-05-15 15:52:03 +0530

    set up express server with dotenv and cors; add nodemon for development

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
 
+dotenv.config()
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4000
+
+// console.log(process.env);
+
+app.use(cors())
 
 app.get("/" , (req, res) => {
     res.send("hello user")
-})
+    }) 
+
+app.get("/:value" , (req, res) => {
+    res.send(`hello ${req.params.value}`)
+}) 
+
+app.get("/piyush" , (req, res) => {
+    res.send("hello grand pituysg")
+    }) 
 
 app.listen(port , ()=>{
     console.log(`app is listening on ${port}`);
